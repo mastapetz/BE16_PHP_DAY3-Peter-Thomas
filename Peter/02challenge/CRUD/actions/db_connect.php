d5eaf15a9e75138a69731eaed47200123d1ca68f
Peter Aigner <mastapetz@icloud.com>
2022-07-13 13:38:42 +0200

    create challenge root and begin code

---
+++ Peter/02challenge/CRUD/actions/db_connect.php
@@ -0,0 +1,18 @@
+<?php 
+
+$hostname = "127.0.0.1"; // this is the hostname that you can find in the PhpMyAdmin and you can write "localhost" too
+$username = "root"; // be default the userName for the databases is root
+$password = ""; // by default there is no password in the databases
+$dbname = "day3_restaurant"; // here we need to write the Database name
+
+// create connection, you need to be aware of the order of the parameters
+$connect = new mysqli($hostname, $username, $password, $dbname);
+
+// check connection
+if($connect->connect_error) {
+   die("Connection failed: " . $connect->connect_error);
+}else {
+
+    echo "Successfully Connected";
+
+}
